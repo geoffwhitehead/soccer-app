764d17a53ffb5773ac16b35791465a7530814b44
Geoff Whitehead <geoff1012@gmail.com>
2016-11-21 02:30:51 +0000

    added some functions to determine whether a team will be relegated or whether they are the champion

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 angular.module('demo')
     .controller('SoccerController', ['soccerFactory', function(soccerFactory) {
     let self = this;
+    let POINTS_FOR_WIN  = 3;
 
     self.season = {};
     self.seasons = [];
@@ -12,6 +13,8 @@ angular.module('demo')
     self._getIndexofSelectedSeason;
     self._markRelegationCandidates;
     self._getTeamInfo;
+    self._isChampion;
+    self._isRelegated;
 
     // get a list of leagues to display in the select box
     soccerFactory.list().then(function(res) {
@@ -22,6 +25,7 @@ angular.module('demo')
     self._getInfo = function(id) {
       soccerFactory.get(id).then(function(res) {
         self.league = res.data;
+        console.log(self.league);
         self._markRelegationCandidates();
         self._getIndexofSelectedSeason(id);
       })
@@ -45,12 +49,49 @@ angular.module('demo')
 
     // fetch the info for selected team.
     self._getTeamInfo = function(link) {
-      var pattern = new RegExp(/[^/]+(?=\/$|$)/);
-      var id = link.match(pattern)[0];
+      let pattern = new RegExp(/[^/]+(?=\/$|$)/);
+      let id = link.match(pattern)[0];
 
       soccerFactory.getTeam(id).then(function(res) {
         self.team = res.data;
       })
+    }
+
+    // determine if leader is champion - assuming the API always return a sorted array
+    self._isChampion = function() {
+      let isChampion = true;
+      let gamesPerTeam = ( self.league.standing.length - 2 ) * 2;
+      let leaderScore = self.league.standing[0].points;
+
+      for (var i = 1; i < self.league.standing.length; i++) {
+        let gamesRemaining = gamesPerTeam - self.league.standing[i].playedGames;
+        let currentPoints = self.league.standing[i].points;
+        let highestPossibleScore = currentPoints + ( POINTS_FOR_WIN * gamesRemaining );
+
+        if (highestPossibleScore >= leaderScore) {
+          isChampion = false;
+        }
+
+        // set variable on leader object
+        if (i == self.league.standing.length) {
+          self.league.standing[0].isChampion == isChampion;
+        }
+      }
+    }
+
+    // determine if relegated - assuming data returned from API is sorted by position
+    self._isRelegated = function() {
+      let gamesPerTeam = ( self.league.standing.length - 2 ) * 2;
+      let pointThreshold = self.league.standing[self.league.standing.length - 4];
+
+      for (var i = self.league.standing.length-1; i > self.league.standing.length - 4; i--) {
+        let gamesRemaining = gamesPerTeam - self.league.standing[i].playedGames;
+        let currentPoints = self.league.standing[i].points;
+        let highestPossibleScore = currentPoints + ( POINTS_FOR_WIN * gamesRemaining );
+        if (highestPossibleScore < pointThreshold) {
+          self.league.standing[i].isRelegated == true;
+        }
+      }
     }
 
   }]);
